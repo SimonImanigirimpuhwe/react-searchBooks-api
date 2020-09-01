64cabc5283f272cfe042a7eb57df3949160eb85d
Simon97i <softimag08@gmail.com>
2020-09-01 13:06:53 +0200

    revert connectionStr

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './route/auth';
-import db from './model/db';
+import './model/db';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/', authRouter);
-db();
-
 
 app.use('/', (req, res) => {
     res.status(200).json({message: 'You reached to psychology books finder'})
--- src/model/db.js
+++ src/model/db.js
@@ -14,10 +14,9 @@ const {
 } = config;
 
  export const url = `mongodb://${host}:${port}/${name}?authSource=admin` || config.db.database_url;
- export default () => {
-      mongoose
+ export default mongoose
         .connect(url, options)
         .then(() => console.log('MongoDB connected...'))
         .catch((err) => {throw new Error(err)})
-}
+
 
